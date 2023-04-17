ab227bac81de1cc0eb7d12f4ba680d8e70d420e5
Arydiane <arydianejardim@gmail.com>
2023-04-17 19:31:56 -0300

    adiciona componente navbar

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Navbar from './components/Navbar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <h1>Trato Tech</h1>
+    <Navbar />
   </React.StrictMode>
 );
 
