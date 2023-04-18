df9798ce14f9e460aff2d5be67e761f10f8e6b06
Arydiane <arydianejardim@gmail.com>
2023-04-18 20:08:46 -0300

    refatora componente header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,18 +1,23 @@
 import styles from './Header.module.scss'
+import TituloSemImagem  from './TituloSemImagem'
+import TituloComImagem from './TituloComImagem'
 
 export default function Header({ titulo, descricao, imagem, className = "" }) {
     return (
-        <header className={`${styles.header} ${className}`}>
-            <div className={styles['header-texto']}>
-                <h1>{titulo}</h1>
-                <h2>{descricao}</h2>
-            </div>
-            <div className={styles['header-imagem']}>
-                <img 
-                    alt={titulo}
-                    src={imagem}
-                />
-            </div>
+        <header className={styles.header}>
+            { titulo && !imagem && 
+                <TituloSemImagem 
+                    titulo={titulo} 
+                    descricao={descricao} 
+                /> 
+            }
+            { titulo && imagem && 
+                <TituloComImagem 
+                    titulo={titulo} 
+                    descricao={descricao} 
+                    imagem={imagem}
+                    className={className}
+                /> }
         </header>
     )
 }
