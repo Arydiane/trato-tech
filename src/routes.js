9bd9e922066537729a299dae2177e137f93d9bd2
Arydiane <arydianejardim@gmail.com>
2023-04-18 16:57:31 -0300

    cria nova rota para página categoria

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 import PaginaPadrao from 'components/PaginaPadrao'
 import Home from 'components/pages/Home'
+import Categoria from 'components/pages/Categoria'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 export default function Router() {
@@ -8,6 +9,7 @@ export default function Router() {
             <Routes>
                 <Route path='/' element={<PaginaPadrao />}>
                     <Route index element={<Home />} />
+                    <Route path='/categoria/:nomeCategoria' element={<Categoria />} />
                 </Route>
             </Routes>
         </BrowserRouter>
