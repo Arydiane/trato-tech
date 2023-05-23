43653b3a5319fff49b3b60c634a5ebe86c39e882
Arydiane <arydianejardim@gmail.com>
2023-05-22 21:21:39 -0300

    atualiza página home
    remove a busca de itens da página

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -6,7 +6,6 @@ import { useDispatch, useSelector } from 'react-redux';
 import Button from 'components/Button';
 import { useEffect } from 'react';
 import { carregarCategorias } from 'store/reducers/categorias';
-import { buscarItens } from 'store/reducers/itens';
 
 export default function Home(){
 
@@ -16,7 +15,6 @@ export default function Home(){
 
     useEffect(() => {
         dispatch(carregarCategorias())
-        dispatch(buscarItens())
     }, [dispatch])
 
     return (
